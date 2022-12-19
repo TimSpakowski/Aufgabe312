a65555f1bb30787ed4908e3140371915e2e2be9c
TimSpakowski <119943071+TimSpakowski@users.noreply.github.com>
2022-12-19 14:55:53 +0100

    Update Getränke.c

--- Getränke.c
+++ Getränke.c
@@ -1,18 +1,58 @@
 #include <stdio.h>
 
-int main() {
+int main(){
+	
+	//Getränke-Automat Version 0.2
+	
+	int  auswahl = 0;
+	float einwurf = 0;
+	printf("\n Getränke Automat v0.2\n");
+	printf("\n Wählen Sie ihr Getränk aus:\n 1) Wasser (0,50 Euro)\n 2) Limonade (1,00 Euro)\n 3) Bier (2,00)\n");
+	printf("\n Geben Sie 1, 2 oder 3 ein: ");
 
-int einwurf=0;
-printf("Getraenke Automat | Bitte werfen sie 1 Euro ein: ");
-scanf("%d", &einwurf);
+	scanf("%d", &auswahl);
 
-if(einwurf == 1) {
-	printf("\nVielen Dank, bitte entmehmen sie ihr Getraenk.\n");
-}else{
-	printf("\nSie haben kein 1 Euro Stueck eingeworfen.\n");
+	int Menge = 0;
+	
+	printf("\n Geben sie die gewünschte Menge ein: ");
+	scanf("%d", &Menge);
+	
+	float Preis = 0;
+	
+	//überprüfe Auswahl
+ 
+	switch(auswahl){  
+		case 1: Preis = 0.5 * Menge;
+			printf("\n Bitte werfen Sie %f Euro ein: ", &Preis);
+			scanf("%f",&einwurf);
+			if(einwurf == Preis){
+				printf("\n Vielen Dank, bitte entnehmen Sie ihr Getränk.\n\n");
+			}
+			else{
+				printf("\n False!\n\n");
+			}
+		break;
+		case 2: Preis = 1 * Menge;
+			printf("\n Bitte werfen Sie %f Euro ein: ", &Preis);
+			scanf("%f",&einwurf);
+			if(einwurf == Preis){
+				printf("\n Vielen Dank, bitte entnehmen Sie ihr Getränk.\n\n");
+			}
+				printf("\n False!\n\n");
+		break;
+		case 3: Preis = 2 * Menge;
+			printf("\n Bitte werfen Sie %f Euro ein: ", &Preis);
+			
+
+			scanf("%f",&einwurf);
+
+			if(einwurf == Preis){
+				printf("\n Vielen Dank, bitte entnehmen Sie ihr Getränk.\n\n");
+			}else{
+				printf("\n False!\n\n");
+			}
+		break;
+		}
+	
+
 }
-
-return 0;
-
-} 
-
